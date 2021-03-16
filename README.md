d4cf75c62a1dd1a048b2f338831c116d07f13787
Gil Barbara <gilbarbara@gmail.com>
2021-03-15 21:21:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# @intera/eslint-config
+
+Intera's shared [ESLint](https://eslint.org/) configuration.
+
+## Setup
+
+1. Install the package.
+```sh
+$ npm install -D @intera/eslint-config
+```
+
+2. Add `"extends": "airbnb"` to your .eslintrc
+
+[Read more on shared configurations.](https://eslint.org/docs/user-guide/configuring/#extending-configuration-files)
+
+## Extras
+
+This configuration is intended to provide consistent rules and pairs well with [prettier-config](https://github.com/by-intera/prettier-config).
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['simple-import-sort', 'sort-destructure-keys'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useUpdateEffect',
+      },
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'], // Side effect imports
+          ['^react$', '^react-dom$', '^react', '^@?\\w'],
+          ['^src/config', '^src/modules', '^src/locales'],
+          ['^src/components', '^src/routes'],
+          ['^src/types'],
+          ['^test'],
+          ['^\\./[^.]'], // './*'
+          ['^\\.\\./'], // '../*'
+        ],
+      },
+    ],
+    'sort-destructure-keys/sort-destructure-keys': ['error', { caseSensitive: false }],
+  },
+};
